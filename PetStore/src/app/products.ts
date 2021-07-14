51003e5d61707b2ce0e36cbbd432d0147cc40373
Eas Chinta <eas.hida@outlook.com>
2021-07-14 11:48:08 +0200

    aaddsa

---
+++ PetStore/src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    name: 'Dog Food',
+    price: 50,
+    description: 'Full of vitamins',
+    image: "https://cdn.pixabay.com/photo/2020/08/20/23/29/dog-5504866__340.jpg"
+  },
+  {
+    name: 'Cat Food',
+    price: 39,
+    description: 'Full of vitamins',
+    image: "https://cdn.pixabay.com/photo/2017/09/16/01/10/food-flat-lay-2754262_960_720.jpg"
+  },
+  {
+    name: 'Horse Food',
+    price: 99,
+    description: 'Full of vitamins',
+    image: "https://media.istockphoto.com/photos/two-horses-eating-hay-picture-id481193245?s=612x612"
+  }
+ ];
